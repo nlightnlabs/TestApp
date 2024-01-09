60072b038674b0585e87c85be6caf3bd48e16918
nlightnlabs <@nlightnlabs.com>
2024-01-09 14:47:20 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [formData, setFormData] = useState({})
+
+  const handleChange=(e)=>{
+    const {name, value} = e.target
+    setFormData({...formData,...{[name]:value}})
+    console.log({...formData,...{[name]:value}})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-container w-100">
+      <div className="d-flex bg-light justify-content-center p-3 w-100">
+      <form>
+        <div className="form-floating">
+          <input name = "name" type="text" className="form-control" value={formData.name} onChange={(e)=>handleChange(e)}></input>
+          <label htmlFor="name" className="form-label" >Name</label>
+        </div>
+
+        <div className="form-floating">
+          <input name = "company" type="text "className="form-control"  value={formData.company} onChange={(e)=>handleChange(e)}></input>
+          <label htmlFor="company" className="form-label">Company</label>
+        </div>
+
+        <div className="d-flex justify-content-center p-3">
+          <button className="btn btn-primary">Send</button>
+        </div>
+
+        <div>
+          {JSON.stringify(formData)}
+        </div>
+
+      </form>
+      </div>
     </div>
   );
 }
