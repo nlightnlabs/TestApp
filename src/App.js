24dc7ce8d1e2243aaa3ea94c37973a4993998966
nlightnlabs <@nlightnlabs.com>
2024-02-15 13:11:53 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
 
-     //Set up local states
-     const [appData, setAppData] = useState([]);
-     const [appName, setAppName] = useState("custom_app_15");
-     const [formData, setFormData] = useState({
-        entity: "",
-        id: ""
-    })
+    //Set up local states
+    const [purchaseReqs, setPurchaseReqs] = useState(null);
 
-    //script to itnegrate FreeAgent library
     const useExternalScript = (src) => {
         useEffect(() => {
             const script = document.createElement('script');
@@ -30,58 +24,70 @@ function App() {
             };
         }, [src]);
     };
-    useExternalScript('https://freeagentsoftware1.gitlab.io/apps/google-maps/js/lib.js');
+     //script to itnegrate FreeAgent library
+     useExternalScript('https://freeagentsoftware1.gitlab.io/apps/google-maps/js/lib.js');
+    
+    //INPUT FROM FREEAGENT Specifiy App to bring in
+    const PURCHASE_REQ_APP = 'custom_app_53';
 
-    //Free agent function to get all records
-    const initializeFreeAgentConnection = async () => {
+    const initializeFreeAgentConnection = () => {
         const FAAppletClient = window.FAAppletClient;
         
         //Initialize the connection to the FreeAgent this step takes away the loading spinner
         const FAClient = new FAAppletClient({
             appletId: 'test-app-iframe',
         });
-
-        if(appName !="" && appName!=null){
-            try {
-
-            let data=[];
-            const response = await FAClient.listEntityValues({
-                entity: appName
-            });
-        
-            response.map(async (record, index) => {
-                let rowData = {};
-                Object.entries(record.field_values).map(([key,value])=>{
-                    rowData = {...rowData,...{[key]:value.display_value}};
-                })
-                data.push(rowData);
-            })
-        
-                console.log(data);
-                setAppData(data)
-
-            }catch(error){
-                console.log(error);
-                return [];
+    
+        //Bridge to access freeagent apps
+        FAClient.listEntityValues({
+            entity: PURCHASE_REQ_APP,
+            limit: 100,
+            fields: [
+                "seq_id",
+                "request_date",
+            ]
+        }, (purchaseReqs) => {
+                console.log('initializeFreeAgentConnection Success!', purchaseReqs);
+            if (purchaseReqs) {
+                setPurchaseReqs(purchaseReqs);
             }
-        }  
+        });
+
+         //OUTPUT 
+        //Function to create a new record/entity in FA app
+        // FAClient.createEntity({
+        //     entity:"requests",
+        //     field_values: {
+        //         request_type: "",
+        //         subject: "",
+        //         requester: "",
+        //     }
+        // })
+
+        //Function to update or delete a record/entity in FA app
+        // FAClient.updateEntity({
+        //     entity:"requests", // app name
+        //     id:"", //What record to update
+        //     field_values: {
+        //         request_type: "",
+        //         subject: "",
+        //         requester: "",
+        //         deleted: false //ONLY USE IF need to delete
+        //     }
+        // })
     };
 
+    const [formData, setFormData] = useState({
+        entity: "",
+        id: ""
+    })
 
-
-    //Function to tigger data fetch Free Agent Data
-    const getData = async (e)=>{
-        setAppName(formData.entity)
-    }
-
-    //Function handle form change
     const handleChange = (e)=>{
         e.preventDefault()
         const {name,value} = e.target
-        setAppName(name)
-
         let key = name.toLowerCase().replaceAll(" ","_")
         setFormData({...formData,...{[key]:value}})
+        console.log({...formData,...{[key]:value}})
     }
 
     return (
@@ -91,24 +97,27 @@ function App() {
         <h2>FreeAgent Iframe Integration Test App</h2>
         
         <div className="d-flex flex-column" style={{margin: "auto", width: "500px"}}>
-            <div className="form-floating mb-3">
-                <input id="entity" name="entity" className="form-control" placeholder="App Name" onChange={(e)=>handleChange(e)}></input>
-                <label htmlFor="entity" className="form-label" style={{color: "lightgray"}}>App Name</label>
-            </div>
-            <div className="form-floating">
-                <input id="id" name="id" className="form-control" placeholder="Record Id" onChange={(e)=>handleChange(e)}></input>
-                <label htmlFor="id" className="form-label" style={{color: "lightgray"}}>Record Id</label>
-            </div>
+            <form>
+                <div className="form-floating mb-3">
+                    <input id="entity" name="entity" className="form-control" placeholder="App Name" onChange={(e)=>handleChange(e)}></input>
+                    <label htmlFor="entity" className="form-label" style={{color: "lightgray"}}>App Name</label>
+                </div>
+                <div className="form-floating">
+                    <input id="id" name="id" className="form-control" placeholder="Record Id" onChange={(e)=>handleChange(e)}></input>
+                    <label htmlFor="id" className="form-label" style={{color: "lightgray"}}>Record Id</label>
+                </div>
+            </form>
             <div className="d-flex justify-content-center">
-                <button className="btn btn-primary" onClick={(e)=>getData()}>Submit</button>
+                <button className="btn btn-primary">Submit</button>
             </div>
         </div>
-        {appData.length>0 &&
-        <div>
-            <div>{appName} Results:</div>
-            {appData.map(record => (<div key={record.seq_id}>{JSON.stringify(record)}</div>))}
-        </div>
-        }
+
+        {purchaseReqs && (
+            purchaseReqs.map(pReq => (
+            <div>{pReq.field_values.description.value}</div>
+            ))
+        )}
+
     </div>
     );
 }
