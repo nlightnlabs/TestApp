1816071bb94e74d76e837077aadeb0078a84fdb5
nlightnlabs <@nlightnlabs.com>
2024-02-18 18:36:01 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
     const [data, setData] = useState(null);
 
-    const initializeFreeAgentConnection = () => {
+    const initializeFreeAgentConnection = async () => {
     const FAAppletClient = window.FAAppletClient;
     
     //Initialize the connection to the FreeAgent this step takes away the loading spinner
@@ -14,15 +14,23 @@ function App() {
     });
 
     //Load list of purchase requests using FAClient
-    FAClient.listEntityValues({
+    // FAClient.listEntityValues({
+    //     entity: "custom_app_53",
+    //     limit: 100,
+    // }, (response) => {
+    //     console.log('initializeFreeAgentConnection Success!', response);
+    //     if (response) {
+    //         setData(response);
+    //     }
+    // });
+
+    const response = await FAClient.listEntityValues({
         entity: "custom_app_53",
         limit: 100,
-    }, (response) => {
-        console.log('initializeFreeAgentConnection Success!', response);
-        if (response) {
-            setData(response);
-        }
     });
+    if (response) {
+        setData(response);
+    }
 };
 
 const useExternalScript = (src) => {
